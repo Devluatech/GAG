88f54984e0b056cfdcde908f6366614cbff52a83
Devluatech <cozenml@gmail.com>
2025-07-17 16:44:14 +0800

    Delete eggrandomizer.lua

--- eggrandomizer.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KuniDevLua/X/refs/heads/main/UpdatedLuaKuni"))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/t5Cuk55fKq/0", true))()
